497e9ae1ee130c25c8e8cb855c22b86f4fab0d68
slfweb <slf.pro@gmail.com>
2017-01-16 20:10:12 +0100

    first commit

---
+++ src/app/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => <div>App</div>
+
+ReactDOM.render(<App />, document.querySelector(".app"));
+
+export default App
---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+module.exports = {
+    entry: path.resolve(__dirname, 'src/app/index.js'),
+    output: {
+        path: __dirname + '/assets',
+        publicPath: '/assets/',
+        filename: 'bundle.js'
+    },
+    module: {
+      loaders: [
+        { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
+      ]
+    },
+    devtool: 'source-map'
+};
